f47f28b32c1adfa319cff6f20c7e444bb129eddc
John Adams <adamsjr8576@gmail.com>
2020-01-09 09:47:51 -0700

    Add images folder and files

---
+++ src/images/images.js
@@ -0,0 +1,11 @@
+import chili from './chili.png';
+import redChili from './redChili.png';
+import veggieBackground from './veggies.jpg';
+
+const images = {
+  chili,
+  redChili,
+  veggieBackground
+}
+
+export default images;
