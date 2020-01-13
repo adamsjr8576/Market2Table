f937e2a4a0730a33578661d8cfb9098827434de6
John Adams <adamsjr8576@gmail.com>
2020-01-13 10:19:14 -0700

    Add to mockData to have multiple zipcodes searchable and each market has its own info object

--- src/mockData.js
+++ src/mockData.js
@@ -1,13 +1,93 @@
 export const farmersMarkets = [
-     {"id":"1002192", "marketname":"1.7 Ballston FRESHFARM Market"},
-     {"id":"22008839", "marketname":"1.9 Ballston Farmers Market"},
-     {"id":"1002291", "marketname":"2.1 Four Mile Run Farmers & Artisans Market"},
-     {"id":"1002181", "marketname":"4.4 Dupont Circle FRESHFARM Market"}
+     {"zipcode":"22202", "id":"1002192", "marketname":"1.7 Ballston FRESHFARM Market"},
+     {"zipcode":"22202", "id":"22008839", "marketname":"1.9 Ballston Farmers Market"},
+     {"zipcode":"22202", "id":"1002291", "marketname":"2.1 Four Mile Run Farmers & Artisans Market"},
+     {"zipcode":"22202", "id":"1002181", "marketname":"4.4 Dupont Circle FRESHFARM Market"},
+     {"zipcode":"80401", "id":"1007465", "marketname":"1.9 Golden Farmers Market"},
+     {"zipcode":"80401", "id":"22002534", "marketname":"5.9 Littleton Farmers Market"},
+     {"zipcode":"80401", "id":"10098876", "marketname":"3.1 Red Creek Farmers Market"},
+     {"zipcode":"80401", "id":"10053847", "marketname":"2.4 Cherry creek fresh market"},
+     {"zipcode":"80401", "id":"22007263", "marketname":"5.0 Golden Arts & Farmers Market"},
+     {"zipcode":"80401", "id":"10099128", "marketname":"2.1 Chief Hosa Farmers Market"},
+     {"zipcode":"80401", "id":"10050162", "marketname":"10.2 Evergreen Farmers market"},
 ]
 
-export const marketInfo = {
-  "GoogleLink":"http://maps.google.com/?q=38.881112%2C%20-77.112179%20(%22Ballston+FRESHFARM+Market%22)",
-  "Address":"901 N Taylor St, Ballston, Virginia, 22203",
-  "Schedule":"June - October Thursday 3:00 PM to 7:00 PM",
-  "Products":"Baked goods; Cheese and/or dairy products; Eggs; Fresh fruit and vegetables; Fresh and/or dried herbs; Honey; Meat; Poultry"
-}
+export const marketInfo = [
+  {
+    "id":"1002192",
+    "GoogleLink":"http://maps.google.com/?q=38.881112%2C%20-77.112179%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"901 N Taylor St, Ballston, Virginia, 22203",
+    "Schedule":"June - October Thursday 3:00 PM to 7:00 PM",
+    "Products":"Baked goods; Cheese and/or dairy products; Eggs; Fresh fruit and vegetables; Fresh and/or dried herbs; Honey; Meat; Poultry"
+  },
+  {
+    "id":"22008839",
+    "GoogleLink":"http://maps.google.com/?q=38.868130%2C%20-77.127394%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"4821 1st St N, Arlington, VA 22203",
+    "Schedule":"June - August Saturday 11:00 AM to 7:00 PM",
+    "Products":"Cheese and/or dairy products; Eggs"
+  },
+  {
+    "id":"1002291",
+    "GoogleLink":"http://maps.google.com/?q=38.895064%2C%20-77.115306%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"4401-4499 20th St N, Arlington, VA 22207",
+    "Schedule":"June - July Monday 9:00 AM to 3:00 PM",
+    "Products":"Fresh fruit and vegetables"
+  },
+  {
+    "id":"1002181",
+    "GoogleLink":"http://maps.google.com/?q=38.853501%2C%20-77.059516%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"2299-2201 S Hayes St, Arlington, VA 22202",
+    "Schedule":"March - September Wednesday 10:00 AM to 2:00 PM",
+    "Products":"Fresh and/or dried herbs"
+  },
+  {
+    "id":"1007465",
+    "GoogleLink":"http://maps.google.com/?q=39.747620%2C%20-105.245514%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"1019 10th St, Golden, CO 80401",
+    "Schedule":"July - September Sunday 10:00 AM to 2:00 PM",
+    "Products":"Eggs; Fresh fruit and vegetables; Fresh and/or dried herb"
+  },
+  {
+    "id":"22002534",
+    "GoogleLink":"http://maps.google.com/?q=39.732572%2C%20-105.200539%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"18198-17966 W 10th Ave, Golden, CO 80401",
+    "Schedule":"June - August Saturday 9:00 AM to 4:00 PM",
+    "Products":"N/A"
+  },
+  {
+    "id":"10098876",
+    "GoogleLink":"http://maps.google.com/?q=39.668383%2C%20-105.174446%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"15538 W Washburn Ave, Lakewood, CO 80228",
+    "Schedule":"May - August Saturday 11:00 AM to 4:00 PM",
+    "Products":"Fresh and/or dried herbs; Honey; Meat; Poultry"
+  },
+  {
+    "id":"10053847",
+    "GoogleLink":"http://maps.google.com/?q=39.628202%2C%20-105.321388%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"Evergreen, CO 80439",
+    "Schedule":"May - August Tuesday 10:00 AM to 2:00 PM",
+    "Products":"Honey"
+  },
+  {
+    "id":"22007263",
+    "GoogleLink":"http://maps.google.com/?q=39.611277%2C%20-105.062523%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"5998-5900 W Long Dr, Littleton, CO 80123",
+    "Schedule":"May - August Thursday 8:00 AM to 1:00 PM",
+    "Products":"N/A"
+  },
+  {
+    "id":"10099128",
+    "GoogleLink":"http://maps.google.com/?q=39.698796%2C%20-105.329410%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"29025-29043 Crossroads Ln, Evergreen, CO 80439",
+    "Schedule":"June - August Saturday 10:00 AM to 3:00 PM",
+    "Products":"N/A"
+  },
+  {
+    "id":"10050162",
+    "GoogleLink":"http://maps.google.com/?q=39.634409%2C%20-105.315716%20(%22Ballston+FRESHFARM+Market%22)",
+    "Address":"Evergreen, CO 80439",
+    "Schedule":"June - July Saturday 10:00 AM to 2:00 PM",
+    "Products":"Baked goods; Cheese and/or dairy products; Eggs; Fresh fruit and vegetables"
+  }
+]
