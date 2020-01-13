3f076804dd6290fd4854c5cdd4907acfc8401f2a
John Adams <adamsjr8576@gmail.com>
2020-01-12 18:33:51 -0700

    Test mapStateToProps for MarketContainer

--- src/containers/MarketContainer/MarketContainer.test.js
+++ src/containers/MarketContainer/MarketContainer.test.js
@@ -109,4 +109,46 @@ describe('MarketContainer', () => {
       expect(wrapper).toMatchSnapshot();
     });
   });
-})
+
+  describe('mapStateToProps', () => {
+    it('should return an object with markets, zipCode, favorites', () => {
+      const mockMarkets = [
+         {
+           id: "1002192",
+           marketname: "Ballston FRESHFARM Market",
+           favorite: false,
+           GoogleLink: "http://maps.google.com/?q=38.881112%2C%20-77.112179%20(%22Ballston+FRESHFARM+Market%22)",
+           Address: "901 N Taylor St, Ballston, Virginia, 22203",
+           Schedule: "June - October Thursday 3:00 PM to 7:00 PM",
+           Products: "Baked goods; Cheese and/or dairy products; Eggs; Fresh fruit and vegetables; Fresh and/or dried herbs; Honey; Meat; Poultry",
+           latitude: "38.881112",
+           longitude: "-77.112179"
+         },
+         {
+           id: "22008839",
+           marketname: "Ballston Farmers Market",
+           favorite: false,
+           GoogleLink: "http://maps.google.com/?q=38.881112%2C%20-77.112179%20(%22Ballston+FRESHFARM+Market%22)",
+           Address: "901 N Taylor St, Ballston, Virginia, 22203",
+           Schedule: "June - October Thursday 3:00 PM to 7:00 PM",
+           Products: "Baked goods; Cheese and/or dairy products; Eggs; Fresh fruit and vegetables; Fresh and/or dried herbs; Honey; Meat; Poultry",
+           latitude: "38.881112",
+           longitude: "-77.112179"
+         }];
+      const mockState = {
+        farmersMarkets: mockMarkets,
+        zipCode: '80401',
+        favorites: []
+      }
+
+      const expected = {
+        markets: mockMarkets,
+        zipCode: '80401',
+        favorites: []
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+});
